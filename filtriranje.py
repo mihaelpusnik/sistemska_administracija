d9a218ab7e886c215c0f05278ceda46b04aae121
mihaelpusnik <mihael.pusnik@student.um.si>
2023-03-30 21:28:13 +0200

    ustvarjanje oblike programa

---
+++ filtriranje.py
@@ -0,0 +1,18 @@
+def my_roberts(slika):
+    #vaša implementacija
+    return slika_robov 
+
+def my_prewitt(slika):
+    #vaša implementacija
+    return slika_robov 
+
+def my_sobel(slika):
+    #vaša implementacija
+    return slika_robov 
+
+def canny(slika, sp_prag, zg_prag):
+    #vaša implementacija
+    return slika_robov 
+
+def spremeni_kontrast(slika, alfa, beta):
+    pass
