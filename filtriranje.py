d886130d674c59369425a61d604eb8e867750fda
mihaelpusnik <mihael.pusnik@student.um.si>
2023-04-01 00:40:14 +0200

    popravil celoten projekt saj so bile funkcije napacno narete

--- filtriranje.py
+++ filtriranje.py
@@ -1,53 +1,97 @@
 import cv2
 import numpy as np
 
-def convolve(image, kernel):
-    # Get the dimensions of the image and kernel
-    img_height, img_width = image.shape[:2]
-    kernel_height, kernel_width = kernel.shape[:2]
 
-    # Compute the padding required around the input image
-    pad_height = kernel_height // 2
-    pad_width = kernel_width // 2
+def my_roberts(slika):
 
-    # Pad the input image with zeros
-    padded_image = np.pad(image, ((pad_height, pad_height), (pad_width, pad_width)), 'constant')
+	# Define Roberts kernels
+	roberts_x = np.array([[0, 1], [-1, 0]])
+	roberts_y = np.array([[1, 0], [0, -1]])
+	h,w = slika.shape
+	# Compute gradient images
+	slika_robov=np.zeros((h,w))
+	# Compute magnitude of gradients
+	for i in range(0, h - 1):
+		for j in range(0, w - 1):
+			# Compute gradient in the y direction
+			gy= (roberts_y[0][0] * slika[i][j]) + (roberts_y[0][1] * slika[i][j + 1]) + \
+				(roberts_y[1][0] * slika[i + 1][j]) + (roberts_y[1][1] * slika[i + 1][j + 1])
 
-    # Compute the output image
-    output = np.zeros_like(image)
-    for y in range(img_height):
-        for x in range(img_width):
-            patch = padded_image[y:y + kernel_height, x:x + kernel_width]
-            output[y, x] = np.sum(patch * kernel)
+			# Compute gradient in the x direction
+			gx = (roberts_x[0][0] * slika[i][j]) + \
+				(roberts_x[0][1] * slika[i][j + 1]) + \
+				(roberts_x[1][0] * slika[i + 1][j]) + \
+				(roberts_x[1][1] * slika[i + 1][j + 1])
 
-    return output
+			slika_robov[i][j] = np.sqrt(gx**2 + gy**2)
 
-def my_roberts(image):
-    roberts_kernel_x = np.array([[1, 0], [0, -1]], dtype=np.float32)
-    roberts_kernel_y = np.array([[0, 1], [-1, 0]], dtype=np.float32)
-    gradient_x = convolve(image, roberts_kernel_x)
-    gradient_y = convolve(image, roberts_kernel_y)
-    output = np.sqrt(np.power(gradient_x, 2) + np.power(gradient_y, 2)).astype(np.uint8)
-    return output
+	# Normalize magnitude image to range [0, 255]
+	slika_robov = cv2.normalize(slika_robov, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+
+	return slika_robov
+
 
 def my_prewitt(image):
-    prewitt_kernel_x = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]], dtype=np.float32)
-    prewitt_kernel_y = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]], dtype=np.float32)
-    gradient_x = convolve(image, prewitt_kernel_x)
-    gradient_y = convolve(image, prewitt_kernel_y)
-    output=np.sqrt(np.power(gradient_x, 2) + np.power(gradient_y, 2)).astype(np.uint8)
-    return output
+	# Define Prewitt kernels
+	prewitt_kernel_x = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+	prewitt_kernel_y = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
+
+	h, w = image.shape
+	slika_robov=np.zeros((h,w))
+
+	for i in range(1, h - 1):
+		for j in range(1, w - 1):
+			# Compute gradient in the x direction
+			gradient_x = np.sum(image[i-1:i+2, j-1:j+2] * prewitt_kernel_x)
+			# Compute gradient in the y direction
+			gradient_y= np.sum(image[i-1:i+2, j-1:j+2] * prewitt_kernel_y)
+			slika_robov[i][j] = np.sqrt(gradient_x**2 + gradient_y**2)
+
+	# Compute magnitude of gradients
+
+	# Normalize magnitude image to range [0, 255]
+	slika_robov = cv2.normalize(slika_robov, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+
+	return slika_robov
 
 
 
-def my_sobel(image):
-    sobel_kernel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=np.float32)
-    sobel_kernel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]], dtype=np.float32)
-    gradient_x = convolve(image, sobel_kernel_x)
-    gradient_y = convolve(image, sobel_kernel_y)
-    output=np.sqrt(np.power(gradient_x, 2) + np.power(gradient_y, 2)).astype(np.uint8)
-    
-    return output
+
+def my_sobel(slika):
+
+	# Define Sobel kernels
+	sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+	sobel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+	h,w = slika.shape
+	# Compute gradient images
+	slika_robov=np.zeros((h,w))
+	# Compute magnitude of gradients
+	for i in range(1, h - 1):
+		for j in range(1, w - 1):
+			# Compute gradient in the y direction
+			gy= (sobel_y[0][0] * slika[i - 1][j - 1]) + (sobel_y[0][1] * slika[i - 1][j]) + \
+				(sobel_y[0][2] * slika[i - 1][j + 1]) + (sobel_y[1][0] * slika[i][j - 1]) + \
+				(sobel_y[1][1] * slika[i][j]) + (sobel_y[1][2] * slika[i][j + 1]) + \
+				(sobel_y[2][0] * slika[i + 1][j - 1]) + (sobel_y[2][1] * slika[i + 1][j]) + \
+				(sobel_y[2][2] * slika[i + 1][j + 1])
+
+			# Compute gradient in the x direction
+			gx = (sobel_x[0][0] * slika[i - 1][j - 1]) + \
+				(sobel_x[0][1] * slika[i - 1][j]) + \
+				(sobel_x[0][2] * slika[i - 1][j + 1]) + \
+				(sobel_x[1][0] * slika[i][j - 1]) + \
+				(sobel_x[1][1] * slika[i][j]) + \
+				(sobel_x[1][2] * slika[i][j + 1]) + \
+				(sobel_x[2][0] * slika[i + 1][j - 1]) + \
+				(sobel_x[2][1] * slika[i + 1][j]) + \
+				(sobel_x[2][2] * slika[i + 1][j + 1])
+
+			slika_robov[i][j] = np.sqrt(gx**2 + gy**2)
+
+	# Normalize magnitude image to range [0, 255]
+	slika_robov = cv2.normalize(slika_robov, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+
+	return slika_robov
 
 
 
@@ -69,16 +113,16 @@ def smooth(image, kernel_size=5, sigma=1):
 
 
 
-imgGray2 = cv2.imread('lenna.png',0)
+imgGray = cv2.imread('lenna.png',0)
 cv2.namedWindow("Slika2")
-cv2.imshow("Slika2", imgGray2)
+cv2.imshow("Slika2", imgGray)
 
-gauss = cv2.GaussianBlur(imgGray2,(15,15),1)
-imgGray = cv2.hconcat((imgGray2,gauss))
+#gauss = cv2.GaussianBlur(imgGray2,(15,15),1)
+#imgGray = cv2.hconcat((imgGray2,gauss))
 
 #imgGray=spremeni_kontrast(imgGray2,1.2,-50)
-cv2.namedWindow("Slika")
-cv2.imshow("Slika", imgGray)
+#cv2.namedWindow("Slika")
+#cv2.imshow("Slika", imgGray)
 
 
 roberts = my_roberts(imgGray)
@@ -93,7 +137,7 @@ sobel = my_sobel(imgGray)
 cv2.namedWindow("my_sobel")
 cv2.imshow("my_sobel", sobel)
 
-can = canny(imgGray,100,200)
+can = canny(imgGray,10,100)
 cv2.namedWindow("canny")
 cv2.imshow("canny", can)
 
